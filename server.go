f0acc71406c1f88c9fa034b97226ce29602ac13b
Benedek László <lacbenedek@gmail.com>
2020-05-01 10:40:50 +0200

    Added loggin

--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"time"
 
@@ -22,12 +24,20 @@ var endtime time.Time
 var stepsize time.Duration
 var limit time.Duration
 var looking bool = false
+var c data.Config
 
 func main() {
-	c := data.GetConfig()
+	c = data.GetConfig()
 	endtime = time.Now()
 	stepsize = time.Duration(c.Stepsize) * time.Second
 	limit = time.Duration(c.Limit) * time.Second
+
+	lf, err := os.OpenFile("./log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("ERR: Can't log!")
+		return
+	}
+	log.SetOutput(lf)
 
 	r := mux.NewRouter()
 	r.HandleFunc(c.Path, postReqMc).Methods("POST")
@@ -35,9 +45,12 @@ func main() {
 }
 
 func postReqMc(w http.ResponseWriter, r *http.Request) {
+
 	creds := Credentials{}
 	err := json.NewDecoder(r.Body).Decode(&creds)
 	if err != nil {
+		log.Println("WARN: Incoming request, but wrong format!")
+		fmt.Println("Incoming request, but wrong format!")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -49,16 +62,19 @@ func postReqMc(w http.ResponseWriter, r *http.Request) {
 			c := bcrypt.CompareHashAndPassword([]byte(users[i].PasswordHash), []byte(creds.Password))
 			if c == nil {
 				go req(users[i])
+				log.Println("INFO: Incoming request: ", creds.Username)
+				fmt.Println("INFO: Incoming request: ", creds.Username)
 				w.WriteHeader(http.StatusAccepted)
 				return
 			}
 		}
 	}
+	log.Println("WARN: Unauthorized request: ", creds.Username)
+	fmt.Println("WARN: Unauthorized request: ", creds.Username)
 	w.WriteHeader(http.StatusUnauthorized)
 }
 
 func req(user data.User) {
-	fmt.Println(user.Username, "requested the server.")
 	if endtime.Sub(time.Now()) <= 0 {
 		endtime = time.Now()
 	}
@@ -67,13 +83,15 @@ func req(user data.User) {
 		endtime = endtime.Add(stepsize)
 	}
 
-	fmt.Println("Added ", stepsize, ", now at ", endtime, ".")
+	fmt.Println("INFO: Added", stepsize, "seconds.", "Service will stop at ", endtime, ".")
+	log.Println("INFO: Added", stepsize, "seconds.", "Service will stop at ", endtime, ".")
 
 	if looking == false {
 		//START
-		fmt.Println("Starting server.")
+		fmt.Println("Starting service.")
 		cmd := exec.Command("systemctl", "start", c.Service)
 		cmd.Start()
+		log.Println("INFO: Started service.")
 		lookForEnd()
 	}
 }
@@ -86,6 +104,7 @@ func lookForEnd() {
 			//STOP
 			fmt.Println("Stopping server.")
 			cmd.Start()
+			log.Println("INFO: Stopped service.")
 			looking = false
 			return
 		}
